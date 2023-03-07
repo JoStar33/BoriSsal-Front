32e12924f422a1d3d844867d4451f69a3ad5ede4
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-07 15:20:04 +0900

    feat: 테스팅 환경 재설정(경로이슈)

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,19 @@ const customJestConfig = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
-  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/', 
+    "<rootDir>/.next/",
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],  
   testPathIgnorePatterns: ['<rootDir>/src/next-auth'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'cjs', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'mjs', 'cjs', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    // Handle module aliases
+    '^@/(.*)$': '<rootDir>/$1',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
