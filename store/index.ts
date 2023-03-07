e7cfdf8a5f31b3f8c8b248fd7771d56469800255
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-07 13:38:39 +0900

    chore: 리듀서 적용

--- store/index.ts
+++ store/index.ts
@@ -1,9 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-
+import userReducer from './user';
 import { persistReducer } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session"; 
 
 const reducers = combineReducers({
+  userStore: userReducer,
 });
 
 const persistConfig: any = {
