27637991ef972a4f2644241f910b07ded6b8c37a
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-07 14:13:01 +0900

    chore: 스토리북 설정 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
+const path = require('path');
 module.exports = {
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../")
+    }
+    return config;
+  },
   "stories": [
     "../**/*.stories.mdx",
     "../**/*.stories.@(js|jsx|ts|tsx)"
@@ -13,5 +21,5 @@ module.exports = {
   "core": {
     "builder": "@storybook/builder-webpack5"
   },
-  "typescript": { "reactDocgen": "false" },
+  "typescript": { "reactDocgen": "false" }
 }
