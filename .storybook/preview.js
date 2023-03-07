8a23564e2f545c845142e3c05169c26e4912b4e0
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-07 18:19:41 +0900

    chore: storybook 테스팅 환경 재구성(라우터)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,5 @@
 import * as NextImage from "next/image";
+import { RouterContext } from "next/dist/shared/lib/router-context"; // next 12
 
 NextImage.defaultProps = {
   unoptimized: true,
@@ -6,6 +7,15 @@ NextImage.defaultProps = {
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  nextRouter: {
+    path: '/', // defaults to `/`
+    asPath: '/', // defaults to `/`
+    query: {}, // defaults to `{}`
+    push() {
+    }, // defaults to using addon actions integration,
+    //   can override any method in the router
+    Provider: RouterContext.Provider,
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
