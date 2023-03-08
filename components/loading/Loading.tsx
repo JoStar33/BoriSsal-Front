a2e7c01d7ddf5ee490b79a0155c154400766ded9
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-08 13:14:42 +0900

    feat: 로딩바 구현

---
+++ components/loading/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './loading.module.scss'
+
+const Loading = () => {
+  return (
+    <div className={styles.loading}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
