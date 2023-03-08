3e69f047f7dec521ae5e1afc9faabb38a34bade9
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-08 18:12:45 +0900

    feat: 타입 변경 반영

--- hooks/auth/useLoginMutation.ts
+++ hooks/auth/useLoginMutation.ts
@@ -3,7 +3,7 @@ import { login } from "@/apis/user/auth";
 import { useRouter } from 'next/router';
 import { loginType } from "@/types/auth";
 import { setUserState } from "@/store/user";
-import { userInfo } from "@/types/user";
+import { userType } from "@/types/user";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store";
 import { AxiosError } from "axios";
@@ -26,8 +26,14 @@ export const useLoginMutation = ({
       dispatch(setUserState({
         id: res.data._id,
         email: res.data.email,
-        nick: res.data.nick
-      } as userInfo));
+        nick: res.data.nick,
+        sns_id: res.data.sns_id,
+        profile_image: res.data.profile_image,
+        user_role: res.data.user_role,
+        created_at: res.data.created_at,
+        user_product_like: res.data.user_product_like,
+        user_bori_gallery_like: res.data.user_bori_gallery_like,
+      } as userType));
       router.push("/");
     },
     onError: (error: AxiosError) => {
--- types/user.ts
+++ types/user.ts
@@ -1,5 +1,15 @@
-export type userInfo = {
+export type userType = {
   id: string;
   email: string;
   nick: string;
+  sns_id: string;
+  profile_image: string;
+  user_role: number;
+  created_at: Date;
+  user_product_like: [];
+  user_bori_gallery_like: [];
 };
+
+export type userProductLikeType = {
+
+}
