f7b80f1a96ed1c5b693c3ebb89230e1cfa73b2ea
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-08 14:10:09 +0900

    chore: 라우터 모킹 전역으로 변경

--- jest.setup.js
+++ jest.setup.js
@@ -5,6 +5,24 @@
 // Learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import { server } from './mocks/server'
+// 라우터 모킹
+jest.mock('next/router', () => ({
+  useRouter() {
+    return ({
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      events: {
+        on: jest.fn(),
+        off: jest.fn()
+      },
+      beforePopState: jest.fn(() => null),
+      prefetch: jest.fn(() => null)
+    });
+  },
+}));
 // Establish API mocking before all tests.
 beforeAll(() => server.listen());
 // Reset any request handlers that we may add during the tests,
