dd1404b7f5d2cd20d1820827932176bf3e6cd3f8
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-13 13:28:10 +0900

    유저 페이지에 비밀번호 변경 적용

--- pages/user-page/UserPage.stories.tsx
+++ pages/user-page/UserPage.stories.tsx
@@ -2,12 +2,14 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import UserPage from './index';
 import { Provider } from 'react-redux';
 import { store }from '@/store';
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 
 export default {
   title: '회원정보 페이지',
   component: UserPage,
 } as ComponentMeta<typeof UserPage>;
 
-const Template: ComponentStory<typeof UserPage> = () => <Provider store={store}><UserPage/></Provider>;
+const Template: ComponentStory<typeof UserPage> = () => <QueryClientProvider client={queryClient}><Provider store={store}><UserPage/></Provider></QueryClientProvider>;
 
 export const UserInfoTest = Template.bind({});
--- pages/user-page/index.tsx
+++ pages/user-page/index.tsx
@@ -1,43 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { RootState } from "@/store";
 import { useSelector } from 'react-redux';
 import { useDeliverAddressQuery } from '@/hooks/user/useDeliverAddressQuery';
 import UserDeliverAddressPart from '@/components/user/UserDeliverAddressPart/UserDeliverAddressPart';
 import styles from './userpage.module.scss';
+import PassWordChangeDialog from '@/components/dialogs/PassWordChangeDialog/PassWordChangeDialog';
 
 const UserPage = () => {
   const { user } = useSelector((state: RootState) => state.userStore);
   const { data } = useDeliverAddressQuery({user_id: user.id});
+  const [dialog, setDialog] = useState(false);
   return (
-    <div className={styles.userpage_container}>
-      <div className={styles.user_image__deliver_info}>
-        <div className={styles.user_image}>
-          <h1>회원정보</h1>
-          {
-            user.profile_image 
-            ? <Image
-                alt=''
-                width={200}
-                height={200}
-                src={user.profile_image}>
-              </Image>
-            : <div className={styles.none_image}></div>
-          }
+    <>
+      {dialog && <PassWordChangeDialog setDialog={setDialog}></PassWordChangeDialog>}
+      <div className={styles.userpage_container}>
+        <div className={styles.user_image__deliver_info}>
+          <div className={styles.user_image}>
+            <h1>회원정보</h1>
+            {
+              user.profile_image 
+              ? <Image
+                  alt=''
+                  width={200}
+                  height={200}
+                  src={user.profile_image}>
+                </Image>
+              : <div className={styles.none_image}></div>
+            }
+          </div>
+          <div className={styles.user_deliver_address}>
+            <h1>배송정보 변경</h1>
+            <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.phone_number} addressType='phone_number' labelInfo='전화번호: '></UserDeliverAddressPart>
+            <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.address} addressType='address' labelInfo='주소: '></UserDeliverAddressPart>
+            <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.address_detail} addressType='address_detail' labelInfo='상세주소: '></UserDeliverAddressPart>
+          </div>
         </div>
-        <div className={styles.user_deliver_address}>
-          <h1>배송정보 변경</h1>
-          <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.phone_number} addressType='phone_number' labelInfo='전화번호: '></UserDeliverAddressPart>
-          <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.address} addressType='address' labelInfo='주소: '></UserDeliverAddressPart>
-          <UserDeliverAddressPart user_id={user.id} addressInfo={data?.data.address_detail} addressType='address_detail' labelInfo='상세주소: '></UserDeliverAddressPart>
+        <div className={styles.user_info_part}>
+          <p>회원 이메일: {user.email}</p>
+          <p>회원 닉네임: {user.nick}</p>
+          <button onClick={()=>setDialog(true)}>비밀번호 변경</button>
         </div>
       </div>
-      <div className={styles.user_info_part}>
-        <p>회원 이메일: {user.email}</p>
-        <p>회원 닉네임: {user.nick}</p>
-        <button>비밀번호 변경</button>
-      </div>
-    </div>
+    </>
   );
 };
 
