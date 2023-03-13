f6d12a640b874e268afb87e469d89b48988540aa
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-13 17:46:29 +0900

    feat: 로그인 이후 사용자바 구현

---
+++ components/user/UserBar/UserBar.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { store } from '@/store';
+import { Provider } from 'react-redux';
+import UserBar from './UserBar';
+
+export default {
+  title: '유저바 테스트',
+  component: UserBar,
+} as ComponentMeta<typeof UserBar>;
+
+const Template: ComponentStory<typeof UserBar> = () => <Provider store={store}><UserBar /></Provider>;
+
+export const UserBarTest = Template.bind({});
--- components/user/UserBar/UserBar.tsx
+++ components/user/UserBar/UserBar.tsx
@@ -1,23 +1,29 @@
 import { RootState } from '@/store';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { BsFillCartFill } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
+import styles from './userbar.module.scss'
 
 const UserBar = () => {
   const { user } = useSelector((state: RootState) => state.userStore)
   return (
-    <div>
-      <BsFillCartFill size={40}></BsFillCartFill>
-      {
-        user.profile_image 
-          ? <Image
-              width={40}
-              height={40}
-              alt=''
-              src={user.profile_image}></Image>
-          : <div></div>
-      }
+    <div className={styles.userbar_container}>
+      <Link href='/cart-page'>
+        <BsFillCartFill size={40}></BsFillCartFill>
+      </Link>
+      <Link className={styles.user_profile_container} href='/user-page'>
+        {
+          user.profile_image 
+            ? <Image
+                width={40}
+                height={40}
+                alt=''
+                src={user.profile_image}></Image>
+            : <div className={styles.non_profile}></div>
+        }
+      </Link>
       <h1>Welcome {user.nick}</h1>
     </div>
   );
