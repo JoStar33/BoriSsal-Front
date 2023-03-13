2e939e17652fd283cac3dfea38e021480c2e262c
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-13 17:46:13 +0900

    feat: 로그인 버튼 구현

---
+++ components/user/LoginButton/LoginButton.stories.tsx
@@ -0,0 +1,13 @@
+import { store } from '@/store';
+import { Provider } from 'react-redux';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import LoginButton from './LoginButton';
+
+export default {
+  title: '로그인버튼 테스트',
+  component: LoginButton,
+} as ComponentMeta<typeof LoginButton>;
+
+const Template: ComponentStory<typeof LoginButton> = () => <Provider store={store}><LoginButton /></Provider>;
+
+export const LoginButtonTest = Template.bind({});
