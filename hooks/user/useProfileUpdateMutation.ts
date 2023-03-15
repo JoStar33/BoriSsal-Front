bdcfa1fa7fa9d4d80770d2f35962929a69f67307
HoCG <rhwkd5012@naver.com>
2023-03-15 22:22:39 +0900

    chore: 사용자 프로필 업데이트시에 바로 즉각 반영되도록 수정

--- hooks/user/useProfileUpdateMutation.ts
+++ hooks/user/useProfileUpdateMutation.ts
@@ -1,9 +1,15 @@
 import { useMutation } from "react-query";
 import { postProfileImage } from "@/apis/user/user";
 import { userProfileUpload } from "@/types/user";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/store";
+import { setUserProfileState } from "@/store/user";
 
 export const useProfileUpdateMutation = (userProfile: userProfileUpload) => {
-  return useMutation(() => {
-    return postProfileImage(userProfile.image, userProfile.user_id);
+  const dispatch = useDispatch<AppDispatch>();
+  return useMutation(() => postProfileImage(userProfile.image, userProfile.user_id), {
+    onSuccess(data) {
+      dispatch(setUserProfileState(data.data.profile_image))
+    },
   });
 };
