31eaef5d5c54ca60a5bbe03839cd45c6c9000a8c
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-15 11:15:00 +0900

    feat: 유저바 테스트코드 구현 및 유저바 로그아웃 버튼 활성화

--- components/user/UserBar/UserBar.tsx
+++ components/user/UserBar/UserBar.tsx
@@ -1,4 +1,9 @@
+import SuccessDialog from '@/components/dialogs/SuccessDialog/SuccessDialog';
+import ValidateDialog from '@/components/dialogs/ValidateDialog/ValidateDialog';
+import Loading from '@/components/loading/Loading/Loading';
+import { useLogoutMutation } from '@/hooks/auth/useLogoutMutation';
 import { RootState } from '@/store';
+import { AxiosError } from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -7,30 +12,49 @@ import { useSelector } from 'react-redux';
 import styles from './userbar.module.scss'
 
 const UserBar = () => {
-  const { user } = useSelector((state: RootState) => state.userStore)
+  const { user } = useSelector((state: RootState) => state.userStore);
+  const { mutate, isError, isLoading, isSuccess, error } = useLogoutMutation();
   return (
-    <div className={styles.userbar_container}>
-      <Link href='/cart-page'>
-        <BsFillCartFill size={40}></BsFillCartFill>
-      </Link>
-      <Link className={styles.user_profile_container} href='/user-page'>
-        {
-          user.profile_image 
-            ? <Image
-                style={{borderRadius: '100px'}}
-                width={40}
-                height={40}
-                alt=''
-                src={process.env.NEXT_PUBLIC_BORI_SSAL_API_URL + user.profile_image}></Image>
-            : <div className={styles.non_profile}></div>
-        }
-      </Link>
-      <div className={styles.name_space}>
-        <p>Welcome</p>
-        <p>{user.nick}</p>
+    <>
+      {
+        isError && <ValidateDialog text={(error as AxiosError).message} ></ValidateDialog>
+      }
+      {
+        isLoading && <Loading></Loading>
+      }
+      {
+        isSuccess && <SuccessDialog text='로그아웃 성공!'></SuccessDialog>
+      }
+      <div className={styles.userbar_container}>
+        <Link href='/cart-page'>
+          <BsFillCartFill size={40}></BsFillCartFill>
+        </Link>
+        <Link className={styles.user_profile_container} href='/user-page'>
+          {
+            user.profile_image 
+              ? <Image
+                  role='profile'
+                  style={{borderRadius: '100px'}}
+                  width={40}
+                  height={40}
+                  alt=''
+                  src={process.env.NEXT_PUBLIC_BORI_SSAL_API_URL + user.profile_image}></Image>
+              : <div
+                  role='non_profile'
+                  className={styles.non_profile}></div>
+          }
+        </Link>
+        <div className={styles.name_space}>
+          <p>Welcome</p>
+          <p>{user.nick}</p>
+        </div>
+        <button 
+          className={styles.logout_button} 
+          onClick={() => mutate()}>
+          Logout
+        </button>
       </div>
-      <button className={styles.logout_button}>Logout</button>
-    </div>
+    </>
   );
 };
 
