f743d2f4f42f3aafdf9a43ad611271efd9d54bc7
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-17 15:16:02 +0900

    feat: 유저페이지 테스트 코드 누락부분 수정

--- pages/user-page/UserPage.test.tsx
+++ pages/user-page/UserPage.test.tsx
@@ -3,10 +3,12 @@ import { waitFor, render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { QueryClientProvider, QueryClient } from "react-query";
 import UserPage from "./index.page";
+import { setUserState } from "@/store/user";
 
 const queryClient = new QueryClient();
 
-test("UserPage 화면 테스트", () => {
+
+const initRender = () => {
   render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
@@ -14,6 +16,28 @@ test("UserPage 화면 테스트", () => {
       </Provider>
     </QueryClientProvider>
   );
+}
+test("UserPage 화면 테스트", () => {
+  initRender();
   const textCheck = screen.getByText("회원 이메일:");
+  expect(textCheck).toBeInTheDocument();
+});
+
+test("UserPage 스토어 데이터 반영 테스트", async () => {
+  initRender();
+  store.dispatch(
+    setUserState({
+      id: "123543",
+      email: "rhwd12@fasdgf.sdfa",
+      nick: "dsa",
+      sns_id: "asdg",
+      profile_image: "",
+      user_role: 0,
+      created_at: new Date(),
+      user_product_like: [],
+      user_bori_gallery_like: [],
+    })
+  );
+  const textCheck = await screen.findByText(/rhwd12@fasdgf.sdfa/);
   expect(textCheck).toBeInTheDocument();
 });
