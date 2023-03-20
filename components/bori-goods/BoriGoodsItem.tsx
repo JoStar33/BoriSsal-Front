44c92c90d7aaa3f849cb05035e62a18c91257fe2
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-20 18:03:31 +0900

    feat: 보리 굿즈 상세 페이지로 이동기능 추가

--- components/bori-goods/BoriGoodsItem.tsx
+++ components/bori-goods/BoriGoodsItem.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 import { AiFillHeart } from 'react-icons/ai';
 import styles from './bori_goods_item.module.scss';
@@ -13,22 +14,24 @@ interface IProps {
 
 const BoriGoodsItem = ({goods_name, bori_goods_image, goods_like, product_price, category_name}: IProps) => {
   return (
-    <div className={styles.bori_goods_item}>
-      <div>
-        <div style={{ position: 'relative', width: "25vw", height: "25vw" }}>
-          <Image src={`${process.env.NEXT_PUBLIC_BORI_SSAL_API_URL}${bori_goods_image}`} alt={goods_name} fill></Image>
+    <Link href={goods_name}>
+      <div className={styles.bori_goods_item}>
+        <div>
+          <div style={{ position: 'relative', width: "25vw", height: "25vw" }}>
+            <Image src={`${process.env.NEXT_PUBLIC_BORI_SSAL_API_URL}${bori_goods_image}`} alt={goods_name} fill></Image>
+          </div>
+          <div className={styles.heart_box}>
+            <AiFillHeart size={25}></AiFillHeart>
+            {goods_like}
+          </div>
         </div>
-        <div className={styles.heart_box}>
-          <AiFillHeart size={25}></AiFillHeart>
-          {goods_like}
+        <div className={styles.bori_goods_info}>
+          <p className={styles.bori_goods_name}>{goods_name}</p>
+          <p className={styles.goods_category_name}>#{category_name}</p>
+          <p className={styles.bori_goods_price}>{product_price}원</p>
         </div>
       </div>
-      <div className={styles.bori_goods_info}>
-        <p className={styles.bori_goods_name}>{goods_name}</p>
-        <p className={styles.goods_category_name}>#{category_name}</p>
-        <p className={styles.bori_goods_price}>{product_price}원</p>
-      </div>
-    </div>
+    </Link>
   );
 };
 
