a42e32ad1bfd459b92b4addbff90b6d5b97f69b2
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-21 13:50:40 +0900

    chore: 에러페이지 분리

---
+++ components/error/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from "next/image";
+import error_bori from '/public/404/404_bori.png';
+import styles from './error_page.module.scss';
+
+interface IProps {
+  errorMessage: string;
+}
+
+const ErrorPage = ({errorMessage}: IProps) => {
+  return (
+    <div className={styles.error_page_container}>
+    <Image
+      width={220}
+      height={400}
+      src={error_bori}
+      alt={errorMessage}></Image>
+    <h1>
+      {
+        errorMessage
+      }
+    </h1>
+  </div>
+  );
+};
+
+export default ErrorPage;
