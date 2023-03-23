1dd67409582a6526caa9503ae71d6f72814c0ed1
HoCG <rhwkd5012@naver.com>
2023-03-23 23:30:28 +0900

    chore: 프리렌더 이슈 해결

--- pages/_app.app.tsx
+++ pages/_app.app.tsx
@@ -1,7 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
 import { store } from "../store";
 import { QueryClientProvider, QueryClient } from "react-query";
@@ -9,17 +7,14 @@ import DefaultHeader from "@/components/common/Header/Header";
 import React from "react";
 
 const queryClient = new QueryClient();
-export let persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <DefaultHeader></DefaultHeader>
-            <Component {...pageProps} />
-          </PersistGate>
+          <DefaultHeader></DefaultHeader>
+          <Component {...pageProps} />
         </Provider>
       </QueryClientProvider>
     </>
--- store/index.ts
+++ store/index.ts
@@ -1,23 +1,14 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user";
 import cartReducer from './cart';
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
 
 const reducers = combineReducers({
   userStore: userReducer,
   cartStore: cartReducer
 });
 
-const persistConfig: any = {
-  key: "root",
-  storage: storage, // 사용할 스토리지를 정의해요.
-};
-
-const persistedReducer = persistReducer(persistConfig, reducers);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: reducers,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
