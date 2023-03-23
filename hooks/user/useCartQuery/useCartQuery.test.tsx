7e119c480f55982c8ec7733d7aaa1c53c26f486c
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-23 15:47:32 +0900

    chore: 배송주소 테스트 코드에 사용자 정보 추가

--- hooks/user/useCartQuery/useCartQuery.test.tsx
+++ hooks/user/useCartQuery/useCartQuery.test.tsx
@@ -5,6 +5,7 @@ import { renderHook, waitFor } from "@testing-library/react";
 import { useCartQuery } from "./useCartQuery";
 import { Provider } from "react-redux";
 import { store } from "@/store";
+import { setUserState } from "@/store/user";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,17 @@ const Wrapper = ({ children }: any) => {
 };
 
 test("useCartQuery 정상동작 확인 테스트", async () => {
+  store.dispatch(setUserState({
+    id: "23",
+    email: "",
+    nick: "",
+    sns_id: "",
+    profile_image: "",
+    user_role: 0,
+    created_at: new Date(),
+    user_bori_goods_like: [],
+    user_bori_gallery_like: []
+  }))
   const { result } = renderHook(
     () => useCartQuery(),
     {
