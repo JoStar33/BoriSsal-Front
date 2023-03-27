fec5fd42bcbf3ad164f10c43730edf14814c97ab
HoCG <rhwkd5012@naver.com>
2023-03-27 23:23:44 +0900

    feat: admin viewer화면 구현

--- components/admin/AdminViewer/AdminViewer.tsx
+++ components/admin/AdminViewer/AdminViewer.tsx
@@ -2,17 +2,18 @@ import { useState } from "react";
 import BoriGalleryRegister from "../BoriGalleryRegister/BoriGalleryRegister";
 import BoriGoodsRegister from "../BoriGoodsRegister/BoriGoodsRegister";
 import OrderController from "../OrderController/OrderController";
+import styles from './admin_viewer.module.scss';
 
 const AdminViewer = () => {
   const [pageState, setPageState] = useState<string>("boriGoods");
   return (
-    <div>
-      <div>
+    <div className={styles.admin_viewer_container}>
+      <div className={styles.viewer_change_button_container}>
         <button onClick={() => setPageState("boriGoods")}>boriGoods</button>
         <button onClick={() => setPageState("boriGallery")}>boriGallery</button>
         <button onClick={() => setPageState("orderControl")}>orderControl</button>
       </div>
-      <div>
+      <div className={styles.admin_viewer}>
         {
           pageState === "boriGoods" && <BoriGoodsRegister/>
         }
