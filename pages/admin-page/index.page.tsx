2fc7c533d68f0c8dcafbed116b43477e6a59321a
HoCG <rhwkd5012@naver.com>
2023-03-27 23:23:56 +0900

    feat: admin-page구현

--- pages/admin-page/index.page.tsx
+++ pages/admin-page/index.page.tsx
@@ -2,12 +2,13 @@ import AdminChecker from "@/components/admin/AdminChecker/AdminChecker";
 import AdminViewer from "@/components/admin/AdminViewer/AdminViewer";
 import { useLoginCheckQuery } from "@/hooks/auth/useLoginCheckQuery/useLoginCheckQuery";
 import { useState } from "react";
+import styles from './admin_page.module.scss';
 
 const AdminPage = () => {
   useLoginCheckQuery();
   const [adminPass, setAdminPass] = useState<boolean>(false);
   return (
-    <div>
+    <div className={styles.admin_page_container}>
       {
         adminPass 
         ? <AdminViewer/>
