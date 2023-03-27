c79d3c5460f31013967ddde1dfa57f89b4eae3a5
HoCG <rhwkd5012@naver.com>
2023-03-27 23:21:31 +0900

    feat: 어드민 체크 화면 구현

--- components/admin/AdminChecker/AdminChecker.tsx
+++ components/admin/AdminChecker/AdminChecker.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction, useRef } from "react";
+import styles from './admin_checker.module.scss';
 
 interface IProps {
   setAdminPass: Dispatch<SetStateAction<boolean>>
@@ -9,11 +10,11 @@ const AdminChecker = ({ setAdminPass }: IProps) => {
   const handleAdminPass = () => {
     if(!adminKeyRef.current)
       return;
-    if(process.env.NEXT_PUBLIC_ADDMIN_KEY === adminKeyRef.current.value)
+    if(process.env.NEXT_PUBLIC_ADMIN_KEY === adminKeyRef.current.value)
       setAdminPass(true);
   }
   return (
-    <div>
+    <div className={styles.admin_checker_container}>
       <h1>어드민 키 입력</h1>
       <input ref={adminKeyRef} type="password" />
       <button onClick={() => handleAdminPass()}>확인하기</button>
