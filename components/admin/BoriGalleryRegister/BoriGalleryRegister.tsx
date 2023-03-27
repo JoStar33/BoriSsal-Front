1fc6f2060d0ce62200ab76bda04a8b7305e54dd9
HoCG <rhwkd5012@naver.com>
2023-03-27 23:23:23 +0900

    feat: 보리 갤러리 등록 페이지 틀 구현

--- components/admin/BoriGalleryRegister/BoriGalleryRegister.tsx
+++ components/admin/BoriGalleryRegister/BoriGalleryRegister.tsx
@@ -1,8 +1,28 @@
 
 const BoriGalleryRegister = () => {
+  const handleOnChangeImage = () => {
+    
+  }
   return (
     <div>
-      
+      <label
+        style={{
+          cursor: "pointer",
+          position: "absolute",
+          top: "150px",
+          left: "150px",
+        }}
+        className="input-file-button"
+        htmlFor="input-file"
+      >
+      </label>
+      <input
+        id="input-file"
+        type="file"
+        onChange={handleOnChangeImage}
+        style={{ display: "none" }}
+        accept="image/png, image/jpeg"
+      />
     </div>
   );
 };
