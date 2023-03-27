7e5275b487ce932f6aaca1630a42f8845af7fe83
HoCG <rhwkd5012@naver.com>
2023-03-27 23:23:01 +0900

    feat: 굿즈 등록 페이지 구현

--- components/admin/BoriGoodsRegister/BoriGoodsRegister.tsx
+++ components/admin/BoriGoodsRegister/BoriGoodsRegister.tsx
@@ -1,9 +1,151 @@
+import ValidateDialog from "@/components/dialogs/ValidateDialog/ValidateDialog";
+import { useCategoryQuery } from "@/hooks/bori-goods/useCategoryQuery/useCategoryQuery";
+import { useRegistBoriGoodsMutation } from "@/hooks/bori-goods/useRegistBoriGoodsMutation/useRegistBoriGoodsMutation";
+import { IPostBoriGoods } from "@/types/boriGoods";
+import Image from "next/image";
+import { useRef, useState } from "react";
+import styles from "./bori_goods_register.module.scss";
 
 const BoriGoodsRegister = () => {
+  let { data: categoryData } = useCategoryQuery();
+  const [image, setImage] = useState<any>("");
+  const [goodsInfo, setGoodsInfo] = useState<IPostBoriGoods>({
+    bori_goods_name: '',
+    bori_goods_price: 0,
+    bori_goods_stock: 0,
+    bori_goods_desc: '',
+  });
+  const [categoryInfo, setCategoryInfo] = useState<string>('');
+  if(!categoryData) {
+    categoryData = [];
+  }
+  const [dialog, setDialog] = useState<boolean>(false);
+  const [dialogText, setDialogText] = useState<string>('');
+  let formData = useRef<FormData>(new FormData());
+  const { mutate } = useRegistBoriGoodsMutation(categoryInfo, goodsInfo, formData.current);
+  const handleOnChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+    const file: File = e.target.files[0];
+    const fr = new FileReader();
+    fr.readAsDataURL(file);
+    fr.onload = (e) => {
+      if (!e.target) return;
+      if (fr.readyState === 2) {
+        setImage(e.target.result);
+        formData.current.append("img", file);
+        console.log(file);
+        console.log(formData.current);
+      }
+    };
+  };
+  const handleSelectLayout = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setCategoryInfo(e.target.value)
+  };
+  const handleOnChangeGoodsInfo = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setGoodsInfo({
+      ...goodsInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleRegistBoriGoods = () => {
+    if(!image) {
+      setDialogText('상품이미지는 반드시 있어야해요!');
+      setDialog(true);
+      return;
+    }
+    if(goodsInfo.bori_goods_name.length < 1) {
+      setDialogText('이런 굿즈 이름을 안 설정하셨는데... 다시 확인해주세요!');
+      setDialog(true);
+      return;
+    }
+    if(goodsInfo.bori_goods_price < 500) {
+      setDialogText('가격은 500원보다 커야해요!');
+      setDialog(true);
+      return;
+    }
+    if(goodsInfo.bori_goods_stock < 2) {
+      setDialogText('재고는 최소 3개는 있어야해요!');
+      setDialog(true);
+      return;
+    }
+    if(goodsInfo.bori_goods_desc.length < 10) {
+      setDialogText('설명을 최소 10글자 이상 써주세요!');
+      setDialog(true);
+      return;
+    }
+    if(!categoryInfo) {
+      setDialogText('카테고리를 반드시 설정해주셔야 해요!');
+      setDialog(true);
+      return;
+    }
+    mutate();
+  }
   return (
-    <div>
-      
-    </div>
+    <>
+      {
+        dialog && 
+        <figure style={{marginLeft: "-5vw"}}>
+          <ValidateDialog setDialog={setDialog} text={dialogText}></ValidateDialog>
+        </figure>
+      }
+      <div className={styles.bori_goods_register_container}>
+        {!image ? (
+          <label className={styles.regist_image} htmlFor="input-file">
+            상품 이미지
+          </label>
+        ) : (
+          <label className={styles.regist_image} htmlFor="input-file">
+            <figure
+              style={{ width: "30vw", height: "30vw", position: "relative" }}
+            >
+              <Image fill src={image} alt="상품 이미지" />
+            </figure>
+          </label>
+        )}
+        <input
+          id="input-file"
+          type="file"
+          onChange={handleOnChangeImage}
+          style={{ display: "none" }}
+          accept="image/png, image/jpeg"
+        />
+        <div className={styles.text_container}>
+          <label htmlFor="goods-name">굿즈명:</label>
+          <input onChange={handleOnChangeGoodsInfo} name="bori_goods_name" id="goods-name" type="text" />
+        </div>
+        <div className={styles.text_container}>
+          <label htmlFor="goods-price">굿즈가격:</label>
+          <input onChange={handleOnChangeGoodsInfo} name="bori_goods_price" id="goods-price" type="text" />
+        </div>
+        <div className={styles.text_container}>
+          <label htmlFor="goods-stock">재고량:</label>
+          <input onChange={handleOnChangeGoodsInfo} name="bori_goods_stock" id="goods-stock" type="number" />
+        </div>
+        <div className={styles.select_container}>
+          <label htmlFor="goods-category">카테고리:</label>
+          <select onChange={handleSelectLayout}>
+            {
+              categoryData.map((category) => 
+                <option 
+                  key={category._id} 
+                  value={category._id}
+                >
+                  {
+                    category.category_name
+                  }
+                </option>)
+            }
+          </select>
+        </div>
+        <div className={styles.content_container}>
+          <p>굿즈 설명</p>
+          <textarea onChange={handleOnChangeGoodsInfo} name="bori_goods_desc"></textarea>
+        </div>
+        <button onClick={handleRegistBoriGoods} className={styles.goods_register_button}>굿즈 등록</button>
+      </div>
+    </>
   );
 };
 
