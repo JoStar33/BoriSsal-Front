0817deba53439d9cad9eec0a38c89348787c0741
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-28 17:34:48 +0900

    feat: 어드민 페이지에 굿즈, 갤러리 각각의 리스트 화면 추가

--- components/admin/AdminViewer/AdminViewer.tsx
+++ components/admin/AdminViewer/AdminViewer.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import BoriGalleryList from "../BoriGalleryList/BoriGalleryList";
 import BoriGalleryRegister from "../BoriGalleryRegister/BoriGalleryRegister";
+import BoriGoodsList from "../BoriGoodsList/BoriGoodsList";
 import BoriGoodsRegister from "../BoriGoodsRegister/BoriGoodsRegister";
 import OrderController from "../OrderController/OrderController";
 import styles from './admin_viewer.module.scss';
@@ -10,7 +12,9 @@ const AdminViewer = () => {
     <div className={styles.admin_viewer_container}>
       <div className={styles.viewer_change_button_container}>
         <button onClick={() => setPageState("boriGoods")}>boriGoods</button>
+        <button onClick={() => setPageState("goodsList")}>goodsList</button>
         <button onClick={() => setPageState("boriGallery")}>boriGallery</button>
+        <button onClick={() => setPageState("galleryList")}>galleryList</button>
         <button onClick={() => setPageState("orderControl")}>orderControl</button>
       </div>
       <div className={styles.admin_viewer}>
@@ -18,7 +22,13 @@ const AdminViewer = () => {
           pageState === "boriGoods" && <BoriGoodsRegister/>
         }
         {
+          pageState === "goodsList" && <BoriGoodsList/>
+        }
+        {
           pageState === "boriGallery" && <BoriGalleryRegister/>
+        }
+        {
+          pageState === "galleryList" && <BoriGalleryList/>
         }
         {
           pageState === "orderControl" && <OrderController/>
