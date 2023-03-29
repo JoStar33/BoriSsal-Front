895fa0ac41e1c7e283f9560de694afcd20160123
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-29 15:39:23 +0900

    feat: 주문 컨트롤러 기능 구현(검색, 날짜별 검색)

--- components/admin/OrderController/OrderController.tsx
+++ components/admin/OrderController/OrderController.tsx
@@ -1,50 +1,102 @@
+import OrderStatusUpdateDialog from "@/components/dialogs/OrderStatusUpdateDialog/OrderStatusUpdateDialog";
 import OrderEmpty from "@/components/order/OrderEmpty/OrderEmpty";
-import { useState } from "react";
+import { useAllOrderQuery } from "@/hooks/order/useAllOrderQuery/useAllOrderQuery";
+import { useRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { AiFillCaretDown } from "react-icons/ai";
+import OrderItem from "../OrderItem/OrderItem";
 import styles from './order_controller.module.scss';
 
 const OrderController = () => {
   const [startDate, setStartDate] = useState<Date | null>(null);
   const [endDate, setEndDate] = useState<Date | null>(null);
+  const [limit, setLimit] = useState<number>(1);
+  const [search, setSearch] = useState<string>('');
+  const [dialog, setDialog] = useState<boolean>(false);
+  const updateOrderId = useRef<string>('');
+  let { data, refetch } = useAllOrderQuery(limit, search, startDate, endDate);
+  if(!data) {
+    data = {
+      order: [],
+      overflow: true
+    };
+  };
+  const showMoreOrder = async () => {
+    setLimit(() => {
+      return limit + 1;
+    });
+    setTimeout(() => {
+      refetch();
+    }, 300);
+  };
+  const handleChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value)
+  };
+  const handleSearch = async () => {
+    setLimit(() => {
+      return 1;
+    });
+    setTimeout(() => {
+      refetch();
+    }, 300);
+  };
   return (
-    <div className={styles.order_controller_container}>
-      <div className={styles.controller_container}>
-        <div className={styles.search_container}>
-          <label htmlFor="search_goods">검색:</label>
-          <input id='search_goods' type="text"/>
-          <button>검색하기</button>
+    <>
+      {
+        dialog && 
+        <figure style={{marginLeft: "-5vw"}}>
+          <OrderStatusUpdateDialog setDialog={setDialog} order_id={updateOrderId.current}/>
+        </figure>
+      }
+      <div className={styles.order_controller_container}>
+        <div className={styles.controller_container}>
+          <div className={styles.search_container}>
+            <label htmlFor="search_goods">검색:</label>
+            <input onChange={handleChangeSearch} id='search_goods' type="text"/>
+          </div>
+          <div className={styles.date_box}>
+            <div className={styles.date_container}>
+              <p>시작일: </p>
+              <DatePicker
+                dateFormat="yyyy-MM-dd"
+                selected={startDate}
+                onChange={date => setStartDate(date)}
+                selectsStart
+                startDate={startDate}
+                endDate={endDate}
+              />
+            </div>
+            <div className={styles.date_container}>
+              <p>종료일: </p>
+              <DatePicker
+                dateFormat="yyyy-MM-dd"
+                selected={endDate}
+                onChange={date => setEndDate(date)} 
+                selectsEnd
+                minDate={startDate}
+                endDate={endDate}
+              />
+            </div>
+            <button className={styles.search_button} onClick={handleSearch}>검색하기</button>
+          </div>
         </div>
-        <div className={styles.date_box}>
-          <div className={styles.date_container}>
-            <p>시작일: </p>
-            <DatePicker
-              dateFormat="yyyy/MM/dd"
-              selected={startDate}
-              onChange={date => setStartDate(date)}
-              selectsStart
-              startDate={startDate}
-              endDate={endDate}
-            />
-          </div>
-          <div className={styles.date_container}>
-            <p>종료일: </p>
-            <DatePicker
-              dateFormat="yyyy/MM/dd"
-              selected={endDate}
-              onChange={date => setEndDate(date)} 
-              selectsEnd
-              minDate={startDate}
-              endDate={endDate}
-            />
-          </div>
-          <button>날짜별 검색하기</button>
+        <div className={styles.list_container}>
+          {
+            data.order.length !== 0
+            ? data.order.map(orderElement => <OrderItem key={orderElement._id} order={orderElement} updateOrderId={updateOrderId} setDialog={setDialog}/>)
+            : <OrderEmpty/>
+          }
+          {
+            !data.overflow && 
+            <button className={styles.more_show_button} onClick={() => showMoreOrder()}>
+              더보기
+              <AiFillCaretDown></AiFillCaretDown>
+            </button>
+          }
         </div>
       </div>
-      <div className={styles.list_container}>
-        <OrderEmpty/>
-      </div>
-    </div>
+    </>
   );
 };
 
