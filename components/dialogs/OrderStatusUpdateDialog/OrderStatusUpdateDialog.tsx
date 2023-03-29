9e84d8cedc600bcd5abfcbebd0d7e66378ff3926
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-03-29 15:38:57 +0900

    feat: 주문상태 업데이트 다이얼로그 구현

--- components/dialogs/OrderStatusUpdateDialog/OrderStatusUpdateDialog.tsx
+++ components/dialogs/OrderStatusUpdateDialog/OrderStatusUpdateDialog.tsx
@@ -1,8 +1,32 @@
+import { useOrderStatusMutation } from '@/hooks/order/useOrderStatusMutation/useOrderStatusMutation';
+import { GrClose } from 'react-icons/gr';
+import styles from './order_status_update_dialog.module.scss';
+interface IProps {
+  order_id: string;
+  setDialog: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
-const OrderStatusUpdateDialog = () => {
+
+const OrderStatusUpdateDialog = ({setDialog, order_id}: IProps) => {
+  const { mutate } = useOrderStatusMutation(order_id);
+  const handleUpdateStatus = (status: string) => {
+    mutate(status);
+    setDialog(false);
+  }
   return (
-    <div>
-      
+    <div className={styles.dialog_background}>
+      <div className={styles.dialog_container}>
+        <div
+          className={styles.default_close_button}
+          onClick={() => setDialog(false)}
+        >
+          <GrClose size={30}></GrClose>
+        </div>
+        <h1>배송상태 변경하기</h1>
+        <button style={{color: "black", backgroundColor: "#ffffff"}} onClick={() => handleUpdateStatus("배송준비")}>배송준비</button>
+        <button style={{color: "white", backgroundColor: "#5B59C1"}} onClick={() => handleUpdateStatus("배송중")}>배송중</button>
+        <button style={{color: "white", backgroundColor: "#4DC667"}} onClick={() => handleUpdateStatus("배송완료")}>배송완료</button>
+      </div>
     </div>
   );
 };
