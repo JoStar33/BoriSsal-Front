130f0d767971bd5965a68a844838f5906812bd3e
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-03 15:52:56 +0900

    chore: 어드민 체크 실패시 다이얼로그가 출력되도록 수정

--- components/admin/AdminChecker/AdminChecker.tsx
+++ components/admin/AdminChecker/AdminChecker.tsx
@@ -1,3 +1,4 @@
+import { useValidateDialog } from "@/hooks/common/useValidateDialog/useValidateDialog";
 import { Dispatch, SetStateAction, useRef } from "react";
 import styles from './admin_checker.module.scss';
 
@@ -6,19 +7,29 @@ interface IProps {
 }
 
 const AdminChecker = ({ setAdminPass }: IProps) => {
-  const adminKeyRef = useRef<HTMLInputElement>(null)
+  const adminKeyRef = useRef<HTMLInputElement>(null);
+  const { dialog, setDialog, dialogText, renderDialog } = useValidateDialog();
   const handleAdminPass = () => {
     if(!adminKeyRef.current)
       return;
-    if(process.env.NEXT_PUBLIC_ADMIN_KEY === adminKeyRef.current.value)
-      setAdminPass(true);
+    if(process.env.NEXT_PUBLIC_ADMIN_KEY !== adminKeyRef.current.value) {
+      dialogText.current = "어드민 키가 일치하지 않습니다. 다시 확인해주세요."
+      setDialog(true);
+      return;
+    }
+    setAdminPass(true);
   }
   return (
-    <div className={styles.admin_checker_container}>
-      <h1>어드민 키 입력</h1>
-      <input ref={adminKeyRef} type="password" />
-      <button onClick={() => handleAdminPass()}>확인하기</button>
-    </div>
+    <> 
+      {
+        dialog && renderDialog()
+      }
+      <div className={styles.admin_checker_container}>
+        <h1>어드민 키 입력</h1>
+        <input ref={adminKeyRef} type="password" />
+        <button onClick={() => handleAdminPass()}>확인하기</button>
+      </div>
+    </>
   );
 };
 
