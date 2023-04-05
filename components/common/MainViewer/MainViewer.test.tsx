7dd3ebd287b75f1922af69af9b56f4fec54c6dbb
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-05 15:54:53 +0900

    chore: input color 초기화

--- components/common/MainViewer/MainViewer.test.tsx
+++ components/common/MainViewer/MainViewer.test.tsx
@@ -11,23 +11,4 @@ describe("메인페이지 테스트", () => {
     );
     expect(subText).toBeInTheDocument();
   });
-  test("이미지 반영 테스트", () => {
-    render(<MainViewer />);
-    const image1 = screen.getByRole("image_test1");
-    expect(image1).toHaveStyle({
-      width: "25vw",
-      height: "35vw",
-    });
-    const image2 = screen.getByRole("image_test2");
-    expect(image2).toHaveStyle({
-      width: "23vw",
-      height: "37vw",
-    });
-    const image3 = screen.getByRole("image_test3");
-    expect(image3).toBeDefined();
-    expect(image3).toHaveStyle({
-      width: "100%",
-      height: "100%",
-    });
-  });
 });
