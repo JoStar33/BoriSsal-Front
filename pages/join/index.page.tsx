93790c1034d9270a20d3cd590b0c36c5f3c643bd
[아이엔소프트] 황호세 <rhwkd5012@naver.com>
2023-04-06 14:40:13 +0900

    chore: 회원가입 화면 ui 개선

--- pages/join/index.page.tsx
+++ pages/join/index.page.tsx
@@ -15,10 +15,8 @@ import { NextSeo } from "next-seo";
 import Image from "next/image";
 import React, { useMemo, useState } from "react";
 import styles from "./join.module.scss";
-import join1 from "/public/login/join1.png";
-import join2 from "/public/login/join2.png";
-import join3 from "/public/login/join3.png";
-import join4 from "/public/login/join4.png";
+import joinImage from '/public/login/join4.png';
+
 
 interface IJoinAccount extends IJoin{
   passwordCheck: string;
@@ -85,24 +83,27 @@ const Join = () => {
         isSuccess && <SuccessDialog text="회원가입에 성공했어요!"></SuccessDialog>
       }
       <div className={styles.join_container}>
-        <h1>회원가입</h1>
+        <div className={styles.join_header}>
+          <figure style={{width: "5vw", height: "7vw", position: "relative"}}>
+            <Image src={joinImage} alt="회원가입을 안내하는 보리의 사진" fill></Image>
+          </figure>
+          <h1>회원가입</h1>
+        </div>
         <div className={styles.join_container__part}>
-          <Image width={80} height={110} alt="이메일 보리" src={join1}></Image>
           <InputPart validate={validateEmail(account.email)} inputLabel="🐶이메일:" inputName="email" textOrPassword="text" onChangeAccount={onChangeAccount}></InputPart>
           <DuplicateCheckPart validate={validateEmail(account.email)} info={account.email} type={true}></DuplicateCheckPart>
         </div>
         <div className={styles.join_container__part}>
-          <Image width={110} height={110} alt="닉네임 보리" src={join2}></Image>
           <InputPart validate={validateNick(account.nick)} inputLabel="🐶닉네임:" inputName="nick" textOrPassword="text" onChangeAccount={onChangeAccount}></InputPart>
           <DuplicateCheckPart validate={validateNick(account.nick)} info={account.nick} type={false}></DuplicateCheckPart>
         </div>
         <div className={styles.join_container__part}>
-          <Image width={80} height={127} alt="비밀번호 보리" src={join3}></Image>
           <InputPart validate={validatePassword(account.password)} inputLabel="🐶비밀번호:" inputName="password" textOrPassword="password" onChangeAccount={onChangeAccount}></InputPart>
+          <div style={{width: "13vw", height: "1vw"}}></div>
         </div>
         <div className={styles.join_container__part}>
-          <Image width={100} height={110} alt="비밀번호 확인 보리" src={join4}></Image>
           <InputPart validate={validatePasswordCheck(account.password, account.passwordCheck)} inputLabel="🐶비밀번호 확인:" inputName="passwordCheck" textOrPassword="password" onChangeAccount={onChangeAccount}></InputPart>
+          <div style={{width: "13vw", height: "1vw"}}></div>
         </div>
         <button aria-label="회원가입 버튼" className={styles.join_button} role="join" onClick={() => join()}>
           회원가입
